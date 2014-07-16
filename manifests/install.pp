e1cf25f3f835b0aea0e1525af535d89f7762400a
David Herman <david.herman@netbrick.cz>
2014-07-16 13:37:43 +0200

    Install jenv from git

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,22 +6,37 @@ define jenv::install (
 	$home_path	= $home ? { '' => "/home/${user}", default => $home }
 	$jenv_path	= "${home_path}/.jenv"
 
-	$jenvrc		= ".jenvrc"
-
 	if ! defined( Class['jenv::dependencies'] ) {
 		require jenv::dependencies
 	}
 
-	exec { "jenv::install::${user}":
-		command		=> "curl -L -s get.jenv.io | bash > /dev/null",
+	file_line { "jenv::install::profile::${user}":
+                path    => "${home_path}/.profile",
+                line    => "[[ -s \"${jenv_path}/bin/jenv-init.sh\" ]] && source \"${jenv_path}/bin/jenv-init.sh\" && source \"${jenv_path}/commands/completion.sh\"",
+        }	
+
+	exec { "jenv::install::download::${user}":
+		command		=> "git clone https://github.com/netbrick/jenv.git ${jenv_path}",
 		user		=> $user,
 		group		=> $group,
 		creates		=> $jenv_path,
-		onlyif		=> "[ ! -d '${home_path}/.jenv' ]",
+		onlyif		=> "[ ! -d '${jenv_path}' ]",
 		environment	=> [ "HOME=${home_path}" ],
 		path		=> [ '/bin', '/usr/bin', '/usr/sbin' ],
 		timeout		=> 150,
 		cwd		=> $home_path,
-		require		=> [ Package['unzip'], Package['curl'] ],
+		notify		=> Exec["jenv::install:init::${user}"],
 	}	
+
+	exec { "jenv::install:init::${user}":
+		command         => "${jenv_path}/bin/jenv-init.sh",
+                user            => $user,
+                group           => $group,
+                environment     => [ "HOME=${home_path}" ],
+                path            => [ '/bin', '/usr/bin', '/usr/sbin' ],
+                refreshonly	=> true,
+		timeout         => 150,
+                cwd             => $home_path,
+		require		=> Exec["jenv::install::download::${user}"],
+        }
 }
